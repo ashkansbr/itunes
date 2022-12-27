bd0749a40580eca5c59140f810cef1d447e4c7f4
ashkansbr <ashkansaberi7@gmail.com>
2022-12-27 20:58:04 +0330

    create social app and modified models

---
+++ social/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from common.basemodels import BaseModel
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Like(BaseModel):
+    profile = models.ForeignKey(User.profile,
+                                on_delete=models.CASCADE,
+                                related_name='social-likes',
+                                related_query_name='social-like'
+                                )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+    class Meta:
+        unique_together = ('content_type', 'profile', 'object_id')
+
+    def __str__(self):
+        return '{}{}'.format(self.content_type, self.profile.name)
+
+
+class Comment(BaseModel):
+    profile = models.ForeignKey(User.profile,
+                                on_delete=models.CASCADE,
+                                related_name='social_comments',
+                                related_query_name='social_comment'
+                                )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+    text = models.TextField()
+
+    class Meta:
+        unique_together = ('object_id', 'content_type', 'profile')
+
+    def __str__(self):
+        return '{}{}'.format(self.content_type, self.profile.name)
+
+
+
+
+
